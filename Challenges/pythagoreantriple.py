cc8a51fc610101dd9260b60244631a1376341b52
Lucas Norman <lucas.normann@gmail.com>
2017-09-13 17:30:16 -0600

    Changes to pythagorean

--- Challenges/pythagoreantriple.py
+++ Challenges/pythagoreantriple.py
@@ -27,20 +27,23 @@ def again():
         print("Okay, bye!\n")
         sys.exit(0)
     elif yesorno == "yes":
-        sides = [raw_input("\n" + "What numbers would you like me to check? > " + "\n")]
-        if sides.count < 3 or sides.count > 3:
-            print("\nPlease enter 3 numbers or try entering three numbers seperated by spaces.\n")
-            return
-        if sides.count == 3:
-            c = max(sides)
-            a = next(sides)
-            b = next(sides)
-            if a**2 + b**2 == c**2:
-                print("It's a pythagorean triple!\n")
-                again()
-            else:
-                print("Sorry, those numbers don't make a pythagorean triple\n")
-                again()
+        sides = raw_input("What numbers would you like me to check? > " + "\n")
+        input_list = sides.split()
+        input_list = [int(i) for i in input_list]
+        for i in input_list:
+            if input_list.count(i) < "3" or input_list.count(i) > "3":
+                print("\nPlease enter 3 numbers or try entering three numbers seperated by spaces.\n")
+                userInput()
+            if input_list.count(i) == "3":
+                c = max(sides)
+                a = next(sides)
+                b = next(sides)
+                if a**2 + b**2 == c**2:
+                    print("It's a pythagorean triple!\n")
+                    again()
+                else:
+                    print("Sorry, those numbers don't make a pythagorean triple\n")
+                    again()
     else:
         print("\nhuh?\n")
         again()
